0b81b5e69f6ba492efe47f10c2c8eb329e9c332f
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-04 23:08:01 -0500

    init rapid-ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
+import { rapidStylesPath, rapidTailwindTheme, rapidPlugin } from "@rapid-web/ui"
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    rapidStylesPath,
+  ],
+  theme: {
+    extend: rapidTailwindTheme({}),
+  },
+  plugins: [rapidPlugin({})],
 }
