11798aa0dc3cc4b45e06714591cac661abd01529
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-04 22:50:51 -0500

    name + setup react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind(), react()],
+})
